d20b927bed65dedd68bc48f4ed71617636a21470
SuhaibQudah <suhaib.qudah@rhinosoft.io>
2024-09-12 14:07:16 +0300

    add pinia and vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import pinia from "./store/pinia.store.ts";
+import router from "./router/index.route.ts";
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(pinia).use(router)
+    .mount('#app')
