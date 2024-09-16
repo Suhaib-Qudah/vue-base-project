7ffbadc80c30a7bdb3ce14cfb4742ef22bcc58f6
SuhaibQudah <suhaib.qudah@rhinosoft.io>
2024-09-16 19:37:45 +0300

    update packages

---
+++ src/common/components/Paginated.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>To be continued...</h1>
+  <div>
+    <slot />
+  </div>
+</template>
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,6 @@
 import axiosInstance from './axios/axios.plugin.ts';
 import vuetify from './vuetify/vuetify.plugin.ts';
 import i18nInstance from './i18n/i18n.plugin.ts';
+import { useTranslation as useI18 } from 'i18next-vue';
 
-export { axiosInstance, vuetify, i18nInstance };
+export { axiosInstance, vuetify, i18nInstance, useI18 };
